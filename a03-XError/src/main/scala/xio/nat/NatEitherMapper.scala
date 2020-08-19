103f7cdb96fa307cd0e6ca143a5cc7b1eb3337eb
djx314 <djx314@sina.cn>
2020-08-19 18:13:21 +0800

    n

---
+++ a03-XError/src/main/scala/xio/nat/NatEitherMapper.scala
@@ -0,0 +1,46 @@
+package xio.nat
+
+trait NatEitherMapper[I <: NatEither, Source, Target] {
+  type O <: NatEither
+  def map(n: I, cv: Source => Target): O
+}
+
+object NatEitherMapper extends NatEitherMapperImplicit1 {
+  self =>
+
+  type Aux[I <: NatEither, OI <: NatEither, Source, Target] = NatEitherMapper[I, Source, Target] { type O = OI }
+
+  def map[N <: NatEither, NN <: NatEither, P1, P2](n: N, cv: P1 => P2)(implicit nm: NatEitherMapper.Aux[N, NN, P1, P2]): NN = nm.map(n, cv)
+
+  implicit def mapperImplicit1[I1, I2]: NatEitherMapper.Aux[NatEitherFirst[I1], NatEitherFirst[I2], I1, I2] =
+    new NatEitherMapper[NatEitherFirst[I1], I1, I2] {
+      override type O = NatEitherFirst[I2]
+      override def map(n: NatEitherFirst[I1], cv: I1 => I2): NatEitherFirst[I2] = new NatEitherFirst(cv(n.one))
+    }
+
+  implicit def mapperImplicit2[I1, I2, II <: NatEither, OO <: NatEither](implicit
+    iImplicit: NatEitherMapper.Aux[II, OO, I1, I2]
+  ): NatEitherMapper.Aux[NatEitherPositive[II, I1], NatEitherPositive[OO, I2], I1, I2] =
+    new NatEitherMapper[NatEitherPositive[II, I1], I1, I2] {
+      override type O = NatEitherPositive[OO, I2]
+      override def map(n: NatEitherPositive[II, I1], cv: I1 => I2): NatEitherPositive[OO, I2] =
+        n.either.fold(pre => new NatEitherPositive(Left(iImplicit.map(pre, cv))), data => new NatEitherPositive(Right(cv(data))))
+    }
+}
+
+trait NatEitherMapperImplicit1 {
+  implicit def mapperImplicit3[I1, II1, I2]: NatEitherMapper.Aux[NatEitherFirst[I1], NatEitherFirst[I1], II1, I2] =
+    new NatEitherMapper[NatEitherFirst[I1], II1, I2] {
+      override type O = NatEitherFirst[I1]
+      override def map(n: NatEitherFirst[I1], cv: II1 => I2): NatEitherFirst[I1] = new NatEitherFirst(n.one)
+    }
+
+  implicit def mapperImplicit4[I1, II1, I2, II <: NatEither, OO <: NatEither](implicit
+    iImplicit: NatEitherMapper.Aux[II, OO, II1, I2]
+  ): NatEitherMapper.Aux[NatEitherPositive[II, I1], NatEitherPositive[OO, I1], II1, I2] =
+    new NatEitherMapper[NatEitherPositive[II, I1], II1, I2] {
+      override type O = NatEitherPositive[OO, I1]
+      override def map(n: NatEitherPositive[II, I1], cv: II1 => I2): NatEitherPositive[OO, I1] =
+        n.either.fold(pre => new NatEitherPositive(Left(iImplicit.map(pre, cv))), data => new NatEitherPositive(Right(data)))
+    }
+}
--- a03-XError/src/test/scala/xio/Test1.scala
+++ a03-XError/src/test/scala/xio/Test1.scala
@@ -1,14 +1,19 @@
 package xio
 
-import xio.nat.{NatEitherFirst, NatEitherSetter}
+import xio.nat.{NatEitherFirst, NatEitherMapper, NatEitherSetter}
 
 object Test1 {
-  type Ei1 = NatEitherFirst.Type[Item2]#Next[Item3]#Next[Item4]
+  type Ei1 = NatEitherFirst.Type[Item2]#Next[Item3]#Next[Item3]#Next[Item4]#Next[Item5]
 
   def main(arr: Array[String]): Unit = {
-    println(NatEitherSetter.set[Ei1](new Item2)) // First(Item2) :: success :: success
-    println(NatEitherSetter.set[Ei1](new Item3)) // Right(Item3) :: success
-    println(NatEitherSetter.set[Ei1](new Item4)) // Right(Item4)
+    println(NatEitherSetter.set[Ei1](new Item2)) // First(Item2) :: success :: success :: success :: success
+    println(NatEitherSetter.set[Ei1](new Item3)) // Right(Item3) :: success :: success
+    println(NatEitherSetter.set[Ei1](new Item4)) // Right(Item4) :: success
+
+    val item3Either = NatEitherSetter.set[Ei1](new Item3)
+    val item4Either = NatEitherSetter.set[Ei1](new Item4)
+    println(NatEitherMapper.map(item3Either, { i: Item3 => new Item5 })) // Right(Item5) :: success :: success
+    println(NatEitherMapper.map(item4Either, { i: Item3 => new Item5 })) // Right(Item4) :: success
   }
 
 }
