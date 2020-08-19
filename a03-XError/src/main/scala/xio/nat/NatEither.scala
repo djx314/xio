28a49dcd8b1b8c103aa308046a29db8f8ed76e61
djx314 <djx314@sina.cn>
2020-08-19 18:20:51 +0800

    j

--- a03-XError/src/main/scala/xio/nat/NatEither.scala
+++ a03-XError/src/main/scala/xio/nat/NatEither.scala
@@ -4,15 +4,12 @@ trait NatEither {
 
   type Next[I] <: NatEither
   type Map[I] <: NatEither
-  type Current
 
 }
 
 class NatEitherFirst[I](val one: I) extends NatEither {
   self =>
   override type Next[I1] = NatEitherPositive[NatEitherFirst[I], I1]
-  override type Map[I1]  = NatEitherFirst[I1]
-  override type Current  = I
 
   override def toString: String = s"First(${one})"
 }
@@ -24,8 +21,6 @@ object NatEitherFirst {
 class NatEitherPositive[Pre <: NatEither, I](val either: Either[Pre, I]) extends NatEither {
   self =>
   override type Next[I1] = NatEitherPositive[NatEitherPositive[Pre, I], I1]
-  override type Map[I1]  = NatEitherPositive[Pre, I1]
-  override type Current  = I
 
   override def toString: String = either.fold(pre => s"${pre} :: success", i => s"Right(${i})")
 }
