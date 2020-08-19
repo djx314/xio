e9ff7b382c87eb11d5995f5ff65ee2be36c20dd3
djx314 <djx314@sina.cn>
2020-08-19 17:28:52 +0800

    N.

--- a03-XError/src/main/scala/xio/nat/NatEither.scala
+++ a03-XError/src/main/scala/xio/nat/NatEither.scala
@@ -6,9 +6,7 @@ trait NatEither {
   type Map[I] <: NatEither
   type Current
 
-  def nextLeft[I]: Next[I]
   def map[I](cv: Current => I): Map[I]
-  def nextRight[I](item: I): Next[I]
 
 }
 
@@ -18,9 +16,7 @@ class NatEitherFirst[I](val one: I) extends NatEither {
   override type Map[I]   = NatEitherFirst[I]
   override type Current  = I
 
-  override def nextLeft[I1]: NatEitherPositive[NatEitherFirst[I], I1]            = new NatEitherPositive(Left(self))
-  override def map[I](cv: Current => I): NatEitherFirst[I]                       = new NatEitherFirst[I](cv(one))
-  override def nextRight[I1](item: I1): NatEitherPositive[NatEitherFirst[I], I1] = new NatEitherPositive(Right(item))
+  override def map[I](cv: Current => I): NatEitherFirst[I] = new NatEitherFirst[I](cv(one))
 
   override def toString: String = s"First(${one})"
 }
@@ -35,17 +31,7 @@ class NatEitherPositive[Pre <: NatEither, I](val either: Either[Pre, I]) extends
   override type Map[I]   = NatEitherPositive[Pre, I]
   override type Current  = I
 
-  override def nextLeft[I1]: NatEitherPositive[NatEitherPositive[Pre, I], I1] =
-    either.fold(
-      pre => new NatEitherPositive(Left(new NatEitherPositive(Left(pre)))),
-      data => new NatEitherPositive(Left(new NatEitherPositive[Pre, I](Right(data))))
-    )
   override def map[I](cv: Current => I): NatEitherPositive[Pre, I] = new NatEitherPositive(either.map(cv))
-  override def nextRight[I1](item: I1): NatEitherPositive[NatEitherPositive[Pre, I], I1] =
-    either.fold(
-      _ => new NatEitherPositive(Right(item)),
-      data => new NatEitherPositive(Left(new NatEitherPositive(Right(data))))
-    )
 
   override def toString: String = either.fold(pre => s"${pre} :: success", i => s"Right(${i})")
 }
