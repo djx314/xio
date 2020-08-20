037664714e5c703869dd8402a7d253e635a8c5c0
djx314 <djx314@sina.cn>
2020-08-20 20:24:28 +0800

    i

--- a02-XLayer/src/main/scala/xio/nat/NatContent.scala
+++ a02-XLayer/src/main/scala/xio/nat/NatContent.scala
@@ -6,9 +6,7 @@ trait NatContent[T1 <: Nat, T2 <: Nat] {
 
 object NatContent {
 
-  implicit def natContentImplicit[N <: Nat, R2 <: Nat](implicit
-    cv1: NatToTag[R2, N]
-  ): NatContent[N, R2] =
+  implicit def natContentImplicit[N <: Nat, R2 <: Nat](implicit cv1: NatToTag[R2, N]): NatContent[N, R2] =
     new NatContent[N, R2] {
       override def content(n: N): R2 = cv1.tag(n)
     }
