6dd86a7077535bc75bca8133bd47436ac3202be4
djx314 <djx314@sina.cn>
2020-08-21 12:23:14 +0800

    p.

--- a04-XIO/xio/src/main/scala/xio/XIO.scala
+++ a04-XIO/xio/src/main/scala/xio/XIO.scala
@@ -1,6 +1,7 @@
 package xio
 
-import xio.nat.has.NatReversePlus
+import xio.nat.error.XErrorReversePlus
+import xio.nat.has.XHasReversePlus
 import zio.ZIO
 
 import scala.language.implicitConversions
@@ -12,13 +13,13 @@ trait XIO[I <: XHas, L <: XError, R] {
     new XIO[I, L, E1] {
       override def zio: ZIO[I, L, E1] = self.zio.map(cv)
     }
-  def flatMap[I1 <: XHas, L1 <: XError, E1](cv: R => XIO[I1, L1, E1])(implicit v: NatReversePlus[I, I1]): XIO[I1#Plus[I], L1#Plus[L], E1] =
+  def flatMap[I1 <: XHas, L1 <: XError, E1](cv: R => XIO[I1, L1, E1])(implicit v: XHasReversePlus[I, I1], n: XErrorReversePlus[L, L1]): XIO[I1#Plus[I], L1#Plus[L], E1] =
     new XIO[I1#Plus[I], L1#Plus[L], E1] {
       override def zio: ZIO[I1#Plus[I], L1#Plus[L], E1] =
         for {
           plus <- ZIO.identity[I1#Plus[I]]
-          r    <- self.zio.provide(v.takeTail(plus)).mapError(e => (throw new Exception): L1#Plus[L])
-          r1   <- cv(r).zio.provide(v.takeHead(plus)).mapError(e => (throw new Exception): L1#Plus[L])
+          r    <- self.zio.provide(v.takeTail(plus)).mapError(e => n.takeTail(e))
+          r1   <- cv(r).zio.provide(v.takeHead(plus)).mapError(e => n.takeHead(e))
         } yield r1
     }
 }
