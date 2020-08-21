6bfada00d895159614e436b9412e3a5d511ddc74
djx314 <djx314@sina.cn>
2020-08-21 16:24:42 +0800

    n

--- a04-XIO/xio-implicit/src/main/scala/xio/nat/has/XHas.scala
+++ a04-XIO/xio-implicit/src/main/scala/xio/nat/has/XHas.scala
@@ -7,19 +7,24 @@ trait XHas extends xio.nat.has.XHasProjection {
   type RePlus[I <: XHas] <: XHas
 
   def plus[I <: XHas](i: I): Plus[I]
+
   def rePlus[I <: XHas](i: I): RePlus[I]
 }
 
 object XHas extends xio.nat.has.XHasProjectionApply
 
 package nat.has {
+
   class XHasZero extends XHas {
     self =>
     override type Plus[I <: XHas] = I
-    override type RePlus[I <: XHas] = I #Plus[XHasZero]
+    override type RePlus[I <: XHas] = I#Plus[XHasZero]
+
     override def plus[I <: XHas](i: I): I = i
-    override def rePlus[I <: XHas](i: I): I #Plus[XHasZero] = i.plus(self)
-    override def toString: String         = "XHasZero"
+
+    override def rePlus[I <: XHas](i: I): I#Plus[XHasZero] = i.plus(self)
+
+    override def toString: String = "XHasZero"
   }
 
   object XHasZero extends XHasZero
@@ -27,11 +32,15 @@ package nat.has {
   class XHasPositive[Tail <: XHas, Head](val tail: Tail, val head: Head) extends XHas {
     self =>
     override type Plus[I <: XHas] = XHasPositive[Tail#Plus[I], Head]
-    type RePlus[I <: XHas] = I#Plus[XHasPositive[Tail,Head]]
-    override def plus[I <: XHas](i: I): XHasPositive[Tail#Plus[I], Head]              = new XHasPositive(tail.plus(i), head)
-    override def rePlus[I <: XHas](i: I): I#Plus[XHasPositive[Tail,Head]] = i.plus(self)
+    override type RePlus[I <: XHas] = I#Plus[XHasPositive[Tail, Head]]
+
+    override def plus[I <: XHas](i: I): XHasPositive[Tail#Plus[I], Head] = new XHasPositive(tail.plus(i), head)
+
+    override def rePlus[I <: XHas](i: I): I#Plus[XHasPositive[Tail, Head]] = i.plus(self)
+
     def get[P](implicit headerFunctor: HeaderFunctor[XHasPositive[Tail, Head], P]): P = headerFunctor.to(self)
 
     override def toString: String = s"${tail} :: ${head}"
   }
+
 }
--- a04-XIO/xio/src/main/scala/xio/nat/error/NatEitherContent.scala
+++ a04-XIO/xio/src/main/scala/xio/nat/error/NatEitherContent.scala
@@ -11,7 +11,7 @@ object NatEitherContent {
     def apply[I1](item: I1)(implicit natEitherSetter: NatEitherSetter[N1, I1]): NatEitherContent[N1] = new NatEitherContent(natEitherSetter.put(item))
   }
 
-  def apply[N1 <: XError]: NatEitherContentApply[N1] = new NatEitherContentApply[N1]
+  def apply[N1 <: XError]: NatEitherContentApply[N1] = new NatEitherContentApply
 
   implicit def natContentImplicit[N <: XError, R2 <: XError](n: NatEitherContent[N])(implicit cv1: NatEitherToTag[N, R2]): NatEitherContent[R2] =
     new NatEitherContent(cv1.tag(n.n))
