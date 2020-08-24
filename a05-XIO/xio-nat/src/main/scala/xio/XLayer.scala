120596bb3d4be4442817d1d13333190fa74e8bb2
djx314 <djx314@sina.cn>
2020-08-25 00:06:24 +0800

    N,

--- a05-XIO/xio-nat/src/main/scala/xio/XLayer.scala
+++ a05-XIO/xio-nat/src/main/scala/xio/XLayer.scala
@@ -13,14 +13,14 @@ trait XLayer[I <: Nat, L <: NatEither, R <: Nat] {
 
   def map[E1 <: Nat](c: R => E1): XLayer[I, L, E1] =
     new XLayer[I, L, E1] {
-      override def zlayer: ZLayer[I, L, E1] = self.zlayer.map(c)
+      override val zlayer: ZLayer[I, L, E1] = self.zlayer.map(c)
     }
 
   def mapError[E1, ESUM <: NatEither](
     n: (E1, NatEitherSetter.NatEitherApply[ESUM]) => ESUM
   )(implicit nm: NatEitherToTag[L, NatEitherPositive[ESUM, E1]]): XLayer[I, ESUM, R] =
     new XLayer[I, ESUM, R] {
-      override def zlayer: ZLayer[I, ESUM, R] = {
+      override val zlayer: ZLayer[I, ESUM, R] = {
         self.zlayer.mapError(l => nm.tag(l).either.fold(identity, e1 => n(e1, new NatEitherSetter.NatEitherApply)))
       }
     }
@@ -29,16 +29,16 @@ trait XLayer[I <: Nat, L <: NatEither, R <: Nat] {
     that: XLayer[RIn2, E1, ROut2]
   )(implicit n: NatToTag[RIn2, R], e: NatEitherReversePlus[L, E1]): XLayer[I, E1#Plus[L], ROut2] =
     new XLayer[I, E1#Plus[L], ROut2] {
-      override def zlayer: ZLayer[I, E1#Plus[L], ROut2] = self.zlayer.mapError(e.takeTail).>>>(ZLayer.identity[R].map(n.tag).>>>(that.zlayer.mapError(e.takeHead)))
+      override val zlayer: ZLayer[I, E1#Plus[L], ROut2] = self.zlayer.mapError(e.takeTail).>>>(ZLayer.requires[R].map(n.tag).>>>(that.zlayer.mapError(e.takeHead)))
     }
 
   def ++[E1 <: NatEither, RIn2 <: Nat, ROut2 <: Nat](
     that: XLayer[RIn2, E1, ROut2]
   )(implicit n1: NatReversePlus[I, RIn2], e: NatEitherReversePlus[L, E1]): XLayer[RIn2#Plus[I], E1#Plus[L], ROut2#Plus[R]] =
     new XLayer[RIn2#Plus[I], E1#Plus[L], ROut2#Plus[R]] {
-      override def zlayer: ZLayer[RIn2#Plus[I], E1#Plus[L], ROut2#Plus[R]] = {
-        val l1: ZLayer[RIn2#Plus[I], E1#Plus[L], R]     = ZLayer.identity[RIn2#Plus[I]].map(r => n1.takeTail(r)).>>>(self.zlayer).mapError(e.takeTail)
-        val l2: ZLayer[RIn2#Plus[I], E1#Plus[L], ROut2] = ZLayer.identity[RIn2#Plus[I]].map(r => n1.takeHead(r)).>>>(that.zlayer).mapError(e.takeHead)
+      override val zlayer: ZLayer[RIn2#Plus[I], E1#Plus[L], ROut2#Plus[R]] = {
+        val l1: ZLayer[RIn2#Plus[I], E1#Plus[L], R]     = ZLayer.requires[RIn2#Plus[I]].map(r => n1.takeTail(r)).>>>(self.zlayer).mapError(e.takeTail)
+        val l2: ZLayer[RIn2#Plus[I], E1#Plus[L], ROut2] = ZLayer.requires[RIn2#Plus[I]].map(r => n1.takeHead(r)).>>>(that.zlayer).mapError(e.takeHead)
         l2.zipWithPar(l1)((s, t) => s.plus(t))
       }
     }
@@ -47,7 +47,7 @@ trait XLayer[I <: Nat, L <: NatEither, R <: Nat] {
     that: XLayer[RIn2, E1, ROut2]
   )(implicit nt: NatToTag[RIn2, R], e: NatEitherReversePlus[L, E1]): XLayer[I, E1#Plus[L], ROut2#Plus[R]] =
     new XLayer[I, E1#Plus[L], ROut2#Plus[R]] {
-      override def zlayer: ZLayer[I, E1#Plus[L], ROut2#Plus[R]] = {
+      override val zlayer: ZLayer[I, E1#Plus[L], ROut2#Plus[R]] = {
         val l1: ZLayer[I, E1#Plus[L], R]     = self.zlayer.mapError(e.takeTail)
         val l2: ZLayer[I, E1#Plus[L], ROut2] = l1.map(nt.tag).>>>(that.zlayer.mapError(e.takeHead))
         l2.zipWithPar(l1)((s, t) => s.plus(t))
@@ -58,9 +58,9 @@ trait XLayer[I <: Nat, L <: NatEither, R <: Nat] {
     that: XLayer[RIn2, E1, ROut2]
   )(implicit n1: NatReversePlus[I, RIn2], e: NatEitherReversePlus[L, E1]): XLayer[RIn2#Plus[I], E1#Plus[L], NatPositive[NatPositive[NatZero, ROut2], R]] =
     new XLayer[RIn2#Plus[I], E1#Plus[L], NatPositive[NatPositive[NatZero, ROut2], R]] {
-      override def zlayer: ZLayer[RIn2#Plus[I], E1#Plus[L], NatPositive[NatPositive[NatZero, ROut2], R]] = {
-        val l1: ZLayer[RIn2#Plus[I], E1#Plus[L], R]     = ZLayer.identity[RIn2#Plus[I]].map(r => n1.takeTail(r)).>>>(self.zlayer).mapError(e.takeTail)
-        val l2: ZLayer[RIn2#Plus[I], E1#Plus[L], ROut2] = ZLayer.identity[RIn2#Plus[I]].map(r => n1.takeHead(r)).>>>(that.zlayer).mapError(e.takeHead)
+      override val zlayer: ZLayer[RIn2#Plus[I], E1#Plus[L], NatPositive[NatPositive[NatZero, ROut2], R]] = {
+        val l1: ZLayer[RIn2#Plus[I], E1#Plus[L], R]     = ZLayer.requires[RIn2#Plus[I]].map(r => n1.takeTail(r)).>>>(self.zlayer).mapError(e.takeTail)
+        val l2: ZLayer[RIn2#Plus[I], E1#Plus[L], ROut2] = ZLayer.requires[RIn2#Plus[I]].map(r => n1.takeHead(r)).>>>(that.zlayer).mapError(e.takeHead)
         l1.zipWithPar(l2)((s, t) => new NatPositive(new NatPositive(NatZero, t), s))
       }
     }
@@ -72,11 +72,17 @@ object XLayer {
     i: XLayer[I1, E1, O1]
   )(implicit cv: NatToTag[I1, I2], v: NatEitherToTag[E1, E2], cv3: NatToTag[O2, O1]): XLayer[I2, E2, O2] =
     new XLayer[I2, E2, O2] {
-      override def zlayer: ZLayer[I2, E2, O2] =
-        ZLayer.identity[I2].map(cv.tag).>>>(i.zlayer).mapError(v.tag).map(cv3.tag)
+      override val zlayer: ZLayer[I2, E2, O2] =
+        ZLayer.requires[I2].map(cv.tag).>>>(i.zlayer).mapError(v.tag).map(cv3.tag)
     }
 
-  def identity[T <: Nat]:XLayer[T, NatEitherZero,T] = new XLayer[T, NatEitherZero,T] {
-    override def zlayer: ZLayer[T, NatEitherZero, T] = ZLayer.identity[T]
-  }
+  def identity[T <: Nat]: XLayer[T, NatEitherZero, T] =
+    new XLayer[T, NatEitherZero, T] {
+      override val zlayer: ZLayer[T, NatEitherZero, T] = ZLayer.identity[T]
+    }
+
+  def requires[A <: Nat]: XLayer[A, NatEitherZero, A] =
+    new XLayer[A, NatEitherZero, A] {
+      override val zlayer: ZLayer[A, NatEitherZero, A] = ZLayer.requires[A]
+    }
 }
---
+++ a05-XIO/xio-nat/src/main/scala/xio/XRuntime.scala
@@ -0,0 +1,93 @@
+package xio
+
+import xio.nat.error.{NatEither, NatEitherPositive, NatEitherZero}
+import xio.nat.has.{Nat, NatPositive, NatToTag, NatZero}
+
+import scala.language.implicitConversions
+import zio._
+import zio.internal.Platform
+
+trait XRuntime[R <: Nat] {
+  self =>
+
+  def zioRuntime: Runtime[R]
+
+  def map[R1 <: Nat](f: R => R1): XRuntime[R1] =
+    new XRuntime[R1] {
+      override val zioRuntime: Runtime[R1] = self.zioRuntime.map(f)
+    }
+
+  def mapPlatform(f: Platform => Platform): XRuntime[R] with Object =
+    new XRuntime[R] {
+      override val zioRuntime: Runtime[R] = self.zioRuntime.mapPlatform(f)
+    }
+
+  final def unsafeRun[R1 <: Nat, E <: NatEither, A](zio: => XIO[R1, E, A])(implicit n: NatToTag[R1, R]): A = self.zioRuntime.map(n.tag).unsafeRun(zio.zio)
+
+  final def unsafeRunTask[R1 <: Nat, A](task: => XIO[R1, NatEitherPositive[NatEitherZero, Throwable], A])(implicit n: NatToTag[R1, R]): A =
+    self.zioRuntime.map(n.tag).unsafeRunTask(task.zio.mapError(_.either.getOrElse(throw new NoSuchElementException("Either.right.get on Left"))))
+
+  final def unsafeRunSync[R1 <: Nat, E <: NatEither, A](zio: => XIO[R1, E, A])(implicit n: NatToTag[R1, R]): Exit[E, A] =
+    self.zioRuntime.map(n.tag).unsafeRunSync(zio.zio)
+
+  final def unsafeRunToFuture[R1 <: Nat, E <: Throwable, A](
+    zio: XIO[R1, NatEitherPositive[NatEitherZero, Throwable], A]
+  )(implicit n: NatToTag[R1, R]): CancelableFuture[A] = {
+    self.zioRuntime.map(n.tag).unsafeRunToFuture(zio.zio.mapError(_.either.getOrElse(throw new NoSuchElementException("Either.right.get on Left"))))
+  }
+
+}
+
+object XRuntime {
+  type XEnv = NatPositive[
+    NatPositive[NatPositive[NatPositive[NatPositive[NatZero, clock.Clock.Service], console.Console.Service], system.System.Service], random.Random.Service],
+    blocking.Blocking.Service
+  ]
+
+  object XEnv {
+
+    val xlive: XEnv = new NatPositive(
+      new NatPositive(
+        new NatPositive(new NatPositive(new NatPositive(NatZero, clock.Clock.Service.live), console.Console.Service.live), system.System.Service.live),
+        random.Random.Service.live
+      ),
+      blocking.Blocking.Service.live
+    )
+
+    val any: XLayer[XEnv, NatEitherZero, XEnv] = XLayer.requires[XEnv]
+
+    val live: XLayer[NatZero, NatEitherZero, XEnv] = {
+      val live1: XLayer[NatZero, NatEitherZero, NatPositive[NatZero, clock.Clock.Service]] =
+        new XLayer[NatZero, NatEitherZero, NatPositive[NatZero, clock.Clock.Service]] {
+          override val zlayer: ZLayer[NatZero, NatEitherZero, NatPositive[NatZero, clock.Clock.Service]] = clock.Clock.live.map(r => new NatPositive(NatZero, r.get))
+        }
+      val live2: XLayer[NatZero, NatEitherZero, NatPositive[NatZero, console.Console.Service]] =
+        new XLayer[NatZero, NatEitherZero, NatPositive[NatZero, console.Console.Service]] {
+          override val zlayer: ZLayer[NatZero, NatEitherZero, NatPositive[NatZero, console.Console.Service]] =
+            console.Console.live.map(r => new NatPositive(NatZero, r.get))
+        }
+      val live3: XLayer[NatZero, NatEitherZero, NatPositive[NatZero, system.System.Service]] =
+        new XLayer[NatZero, NatEitherZero, NatPositive[NatZero, system.System.Service]] {
+          override val zlayer: ZLayer[NatZero, NatEitherZero, NatPositive[NatZero, system.System.Service]] = system.System.live.map(r => new NatPositive(NatZero, r.get))
+        }
+      val live4: XLayer[NatZero, NatEitherZero, NatPositive[NatZero, random.Random.Service]] =
+        new XLayer[NatZero, NatEitherZero, NatPositive[NatZero, random.Random.Service]] {
+          override val zlayer: ZLayer[NatZero, NatEitherZero, NatPositive[NatZero, random.Random.Service]] = random.Random.live.map(r => new NatPositive(NatZero, r.get))
+        }
+      val live5: XLayer[NatZero, NatEitherZero, NatPositive[NatZero, blocking.Blocking.Service]] =
+        new XLayer[NatZero, NatEitherZero, NatPositive[NatZero, blocking.Blocking.Service]] {
+          override val zlayer: ZLayer[NatZero, NatEitherZero, NatPositive[NatZero, blocking.Blocking.Service]] =
+            blocking.Blocking.live.map(r => new NatPositive(NatZero, r.get))
+        }
+      live1 ++ live2 ++ live3 ++ live4 ++ live5
+    }
+
+  }
+
+  lazy val default: XRuntime[XEnv] = new XRuntime[XEnv] {
+    override val zioRuntime: Runtime[XEnv] = Runtime(XEnv.xlive, Platform.default)
+  }
+  lazy val global: XRuntime[XEnv] = new XRuntime[XEnv] {
+    override val zioRuntime: Runtime[XEnv] = Runtime(XEnv.xlive, Platform.global)
+  }
+}
