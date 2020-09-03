284313af11623205afa0a3ee58d4a8de6f01cfc4
djx314 <djx314@sina.cn>
2020-09-03 14:06:21 +0800

    n

--- a05-XIO/xio/src/main/scala/xio/nat/has/NatFinder.scala
+++ a05-XIO/xio/src/main/scala/xio/nat/has/NatFinder.scala
@@ -4,27 +4,16 @@ trait NatFinder[NN <: Nat, HeadR] {
   def to(t: NN): HeadR
 }
 
-object NatFinder {
-  implicit def implicit1[HeadT, N <: Nat](implicit n: NatFinderImpl[N#InnerPlus[NatZero], HeadT]): NatFinder[N, HeadT] =
-    new NatFinder[N, HeadT] {
-      override def to(t: N): HeadT = n.to(t.innerPlus(NatZero))
-    }
-}
-
-trait NatFinderImpl[NN <: Nat, HeadR] {
-  def to(t: NN): HeadR
-}
-
-object NatFinderImpl extends NatFinderImplImplicit1 {
-  implicit def implicit1[HeadT, Tail <: Nat]: NatFinderImpl[NatPositive[Tail, HeadT], HeadT] =
-    new NatFinderImpl[NatPositive[Tail, HeadT], HeadT] {
+object NatFinder extends NatFinderImplicit1 {
+  implicit def implicit1[HeadT, Tail <: Nat]: NatFinder[NatPositive[Tail, HeadT], HeadT] =
+    new NatFinder[NatPositive[Tail, HeadT], HeadT] {
       override def to(t: NatPositive[Tail, HeadT]): HeadT = t.head
     }
 }
 
-trait NatFinderImplImplicit1 {
-  implicit def implicit2[HeadT, HeadR, Tail <: Nat](implicit tailH: NatFinderImpl[Tail, HeadR]): NatFinderImpl[NatPositive[Tail, HeadT], HeadR] =
-    new NatFinderImpl[NatPositive[Tail, HeadT], HeadR] {
+trait NatFinderImplicit1 {
+  implicit def implicit2[HeadT, HeadR, Tail <: Nat](implicit tailH: NatFinder[Tail, HeadR]): NatFinder[NatPositive[Tail, HeadT], HeadR] =
+    new NatFinder[NatPositive[Tail, HeadT], HeadR] {
       override def to(t: NatPositive[Tail, HeadT]): HeadR = tailH.to(t.tail)
     }
 }
