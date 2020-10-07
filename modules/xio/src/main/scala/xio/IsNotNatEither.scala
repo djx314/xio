63093ac23996bb89f3b23cc0b51cf465170fa383
djx314 <djx314@sina.cn>
2020-10-07 22:10:30 +0800

    afewtt

--- modules/xio/src/main/scala/xio/IsNotNatEither.scala
+++ modules/xio/src/main/scala/xio/IsNotNatEither.scala
@@ -2,12 +2,28 @@ package xio
 
 import xio.nat.error.NatEither
 
-class IsNotNatEither[T]
+class IsNotNatEitherOrNothing[T]
 
-object IsNotNatEither {
-  val any: IsNotNatEither[Any]          = new IsNotNatEither[Any]
-  def anyInstance[T]: IsNotNatEither[T] = any.asInstanceOf[IsNotNatEither[T]]
+object IsNotNatEitherOrNothing {
+  val any: IsNotNatEitherOrNothing[Any]          = new IsNotNatEitherOrNothing[Any]
+  def anyInstance[T]: IsNotNatEitherOrNothing[T] = any.asInstanceOf[IsNotNatEitherOrNothing[T]]
 
-  implicit def isNotNatEither1[T]: IsNotNatEither[T]              = anyInstance
-  implicit def isNotNatEither2[T <: NatEither]: IsNotNatEither[T] = anyInstance
+  implicit def isNotNatEither1[T]: IsNotNatEitherOrNothing[T]              = anyInstance
+  implicit def isNotNatEither2[T <: NatEither]: IsNotNatEitherOrNothing[T] = anyInstance
+  implicit val isNotNatEither3: IsNotNatEitherOrNothing[Nothing] = anyInstance
+}
+
+class IsNatEither[T]
+
+object IsNatEither {
+  val any: IsNatEither[Any]          = new IsNatEither[Any]
+  def anyInstance[T]: IsNatEither[T] = any.asInstanceOf[IsNatEither[T]]
+
+  implicit def isNotNatEither1[T <: NatEither]: IsNatEither[T] = anyInstance
+}
+
+class IsNothing[T]
+
+object IsNothing {
+  implicit val isNotNatEither1: IsNothing[Nothing]              = new IsNothing[Nothing]
 }
