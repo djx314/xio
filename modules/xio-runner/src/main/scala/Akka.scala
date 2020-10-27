6b8460cdfec2ef0d263819474744c5c1cf2547be
djx314 <djx314@sina.cn>
2020-10-27 12:36:42 +0800

    sfdsdfsf

--- modules/xio-runner/src/main/scala/Akka.scala
+++ modules/xio-runner/src/main/scala/Akka.scala
@@ -3,10 +3,8 @@ package xio.akka.runner
 import java.util
 
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.scaladsl.LoggerOps
 import akka.actor.typed._
 import akka.actor.typed.scaladsl.AskPattern._
-import akka.pattern.Patterns
 import akka.util.Timeout
 import zio._
 
@@ -14,27 +12,22 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.reflect.ClassTag
 
 object HelloWorld {
-  final case class RunResult(value: Any)
+  final case class RunResult(value: Either[Throwable, Any])
   sealed trait QueueReceive
   final case class InputZIO(zio: ZIO[ZEnv, Throwable, Any], replyTo: ActorRef[RunResult]) extends QueueReceive
-  final case class Greeted(from: ActorRef[Greet])                                         extends QueueReceive
+  final case class Greeted(from: ActorRef[InputZIO])                                      extends QueueReceive
 
-  final case class StartZIO(replyTo: ActorRef[Greeted])
-  final case class Greet(zio: ZIO[ZEnv, Throwable, Any], replyTo: ActorRef[RunResult])
-
-  def apply(): Behavior[StartZIO] = Behaviors.receive { (context, message) =>
-    // context.log.info("Hello {}!", message.whom)
-    // message.replyTo ! Greeted(message.whom, context.self)
+  def fromActorRef(replyTo: ActorRef[Greeted])(actorSystem: ActorSystem[Nothing]): CancelableFuture[Int] = {
 
     import scala.concurrent.duration._
     import scala.util._
     implicit val timeOut: Timeout             = 10.seconds
-    implicit val system: ActorSystem[Nothing] = context.system
-    implicit val ec: ExecutionContext         = context.system.executionContext
+    implicit val system: ActorSystem[Nothing] = actorSystem
+    implicit val ec: ExecutionContext         = actorSystem.executionContext
 
     def forZIO: ZIO[ZEnv, Throwable, Int] = {
-      val ii = ZIO.effectAsync[ZEnv, Throwable, Greet] { result =>
-        message.replyTo.ask((i: ActorRef[Greet]) => Greeted(i)).onComplete {
+      val ii = ZIO.effectAsync[ZEnv, Throwable, InputZIO] { result =>
+        replyTo.ask((i: ActorRef[InputZIO]) => Greeted(i)).onComplete {
           case Success(i) => result(ZIO.succeed(i))
           case Failure(e) => result(ZIO.fail(e))
         }
@@ -42,32 +35,41 @@ object HelloWorld {
       for {
         inner   <- ii
         result1 <- forZIO.fork
-        result2 <- inner.zio.fork
+        result2 <- inner.zio.either.fork
         result3 <- result2.join
         result4 = inner.replyTo ! RunResult(result3)
         i <- result1.join
-      } yield i
+      } yield 2
     }
 
     Runtime.default.unsafeRunToFuture(forZIO)
-    Behaviors.same
-
   }
+
 }
 
 object HelloWorldBot {
 
-  val queue: java.util.Queue[HelloWorld.Greet] = new util.LinkedList[HelloWorld.Greet]
+  val queue: java.util.Queue[HelloWorld.InputZIO] = new util.LinkedList[HelloWorld.InputZIO]
+  var replyTo: HelloWorld.Greeted                 = null
 
   def apply(): Behavior[HelloWorld.QueueReceive] =
     Behaviors.receive { (context, message) =>
       message match {
-        case HelloWorld.InputZIO(i1, i2) =>
-          queue.offer(HelloWorld.Greet(i1, i2))
-        case i @ HelloWorld.Greeted(_) =>
+        case in: HelloWorld.InputZIO =>
+          queue.offer(in)
+          if (replyTo != null) {
+            val ele = queue.poll()
+            if (ele != null) {
+              replyTo.from ! ele
+              replyTo = null
+            }
+          }
+        case i: HelloWorld.Greeted =>
           val ele = queue.poll()
           if (ele != null) {
             i.from ! ele
+          } else {
+            replyTo = i
           }
       }
       Behaviors.same
@@ -78,17 +80,15 @@ object HelloWorldMain {
 
   final case class SayHello(zioTask: ZIO[ZEnv, Throwable, Any], replyTo: ActorRef[HelloWorld.RunResult])
 
-  def apply(): Behavior[SayHello] =
-    Behaviors.setup { context =>
-      val greeter = context.spawnAnonymous(HelloWorld())
-      val replyTo = context.spawnAnonymous(HelloWorldBot.apply())
-      greeter ! HelloWorld.StartZIO(replyTo)
+  def apply(): Behavior[SayHello] = Behaviors.setup { context =>
+    val replyTo = context.spawn(HelloWorldBot.apply(), "zio-sender")
+    HelloWorld.fromActorRef(replyTo)(context.system)
 
-      Behaviors.receiveMessage { message =>
-        replyTo ! HelloWorld.InputZIO(message.zioTask, message.replyTo)
-        Behaviors.same
-      }
+    Behaviors.receiveMessage { message =>
+      replyTo ! HelloWorld.InputZIO(message.zioTask, message.replyTo)
+      Behaviors.same
     }
+  }
 
   def main(args: Array[String]): Unit = {
     implicit val system: ActorSystem[HelloWorldMain.SayHello] = ActorSystem(HelloWorldMain(), "hello")
@@ -99,13 +99,13 @@ object HelloWorldMain {
     implicit val ec: ExecutionContext = system.executionContext
 
     def runToFuture[T](zio: ZIO[ZEnv, Throwable, T])(implicit tag: ClassTag[T]): Future[T] =
-      system.ask((ac: ActorRef[HelloWorld.RunResult]) => HelloWorldMain.SayHello(zio, ac)).map(r => r.value).mapTo[T]
+      system.ask((ac: ActorRef[HelloWorld.RunResult]) => HelloWorldMain.SayHello(zio, ac)).flatMap(r => r.value.fold(Future.failed, Future.successful)).mapTo[T]
 
     val aa = (1 to 1000)
       .to(List)
       .map(i =>
         for {
-          _ <- clock.sleep(zio.duration.Duration.fromMillis(Random.nextLong(6000)))
+          _ <- clock.sleep(zio.duration.Duration.fromMillis(Random.nextLong(3000)))
         } yield i
       )
 
