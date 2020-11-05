c275dea568c9bd4befc98a6bad8ce480a4cf4455
djx314 <djx314@sina.cn>
2020-11-05 10:34:56 +0800

    srewrwr

--- modules/xio/src/main/scala/xio/helper/AllToZIO.scala
+++ modules/xio/src/main/scala/xio/helper/AllToZIO.scala
@@ -1,18 +1,18 @@
 package xio.helper
 import xio.nat.error.NatEitherToTag
 
-trait AllToZIO[In, R, E, A] {
+trait AllToZIO[In, -R, +E, +A] {
   def input(in: In): zio.ZIO[R, E, A]
 }
 
 object AllToZIO {
-  implicit def implicit1[R1, A1, Err <: xio.nat.error.NatEither, R <: R1, E, A >: A1](implicit
+  implicit def implicit1[Err <: xio.nat.error.NatEither, R, E, A](implicit
     lift: NatEitherToTag[Err, xio.XError1[E]]
-  ): AllToZIO[xio.XIO[R1, Err, A1], R, E, A] = new AllToZIO[xio.XIO[R1, Err, A1], R, E, A] {
-    override def input(in: xio.XIO[R1, Err, A1]): zio.ZIO[R, E, A] = in.mapError(d => lift.tag(d).sureRight)
+  ): AllToZIO[xio.XIO[R, Err, A], R, E, A] = new AllToZIO[xio.XIO[R, Err, A], R, E, A] {
+    override def input(in: xio.XIO[R, Err, A]): zio.ZIO[R, E, A] = in.mapError(d => lift.tag(d).sureRight)
   }
-  implicit def implicit2[R1, A1, R <: R1, E, A >: A1]: AllToZIO[zio.ZIO[R1, E, A1], R, E, A] = new AllToZIO[zio.ZIO[R1, E, A1], R, E, A] {
-    override def input(in: zio.ZIO[R1, E, A1]): zio.ZIO[R, E, A] = in
+  implicit def implicit2[R, E, A]: AllToZIO[zio.ZIO[R, E, A], R, E, A] = new AllToZIO[zio.ZIO[R, E, A], R, E, A] {
+    override def input(in: zio.ZIO[R, E, A]): zio.ZIO[R, E, A] = in
   }
   implicit def implicit3[R, E, A]: AllToZIO[E, R, E, A] = new AllToZIO[E, R, E, A] {
     override def input(in: E): zio.ZIO[R, E, A] = zio.ZIO.fail(in)
