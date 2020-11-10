95329c01e0d16abee708cdf7796a7548535e9a7d
djx314 <djx314@sina.cn>
2020-11-10 09:04:08 +0800

    serewrwer

--- modules/xio-runner/src/main/scala/ActionQueue.scala
+++ modules/xio-runner/src/main/scala/ActionQueue.scala
@@ -11,26 +11,26 @@ class ActionQueue {
   val queue: java.util.Queue[ActorToZIO.InputZIO] = new util.LinkedList[ActorToZIO.InputZIO]
   var replyTo: ActorToZIO.ReceiveZIOAction        = null
 
-  def apply(): Behavior[ActorToZIO.QueueReceive] =
-    Behaviors.receive { (context, message) =>
-      message match {
-        case in: ActorToZIO.InputZIO =>
-          queue.offer(in)
-          if (replyTo != null) {
-            val ele = queue.poll()
-            if (ele != null) {
-              replyTo.from(ZIO.succeed(ele))
-              replyTo = null
-            }
-          }
-        case i: ActorToZIO.ReceiveZIOAction =>
+  def apply(): Behavior[ActorToZIO.QueueReceive] = Behaviors.receive { (context, message) =>
+    message match {
+      case in: ActorToZIO.InputZIO =>
+        println("接收到一个 zio action")
+        queue.offer(in)
+        if (replyTo != null) {
           val ele = queue.poll()
           if (ele != null) {
-            i.from(ZIO.succeed(ele))
-          } else {
-            replyTo = i
+            replyTo.from(ZIO.succeed(ele))
+            replyTo = null
           }
-      }
-      Behaviors.same
+        }
+      case i: ActorToZIO.ReceiveZIOAction =>
+        val ele = queue.poll()
+        if (ele != null) {
+          i.from(ZIO.succeed(ele))
+        } else {
+          replyTo = i
+        }
     }
+    Behaviors.same
+  }
 }
